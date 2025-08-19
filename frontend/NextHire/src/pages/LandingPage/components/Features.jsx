f12ab3eb5013a9a35d618ef2f3a9f3db38ee2768
Bruno Lopes <bruno-lopes9@hotmail.com>
2025-08-19 09:40:36 +0100

    Add features.jsx, and put Utils in src directory

--- frontend/NextHire/src/pages/LandingPage/components/Features.jsx
+++ frontend/NextHire/src/pages/LandingPage/components/Features.jsx
@@ -1,7 +1,69 @@
 import React from "react"
+import { employerFeatures, jobSeekerFeatures } from "../../../utils/data"
 
 const Features = () => {
-  return <div> Features </div>
+  return (
+    <section className="">
+      <div className="">
+        <div className="">
+          <h2 className="">
+            Everything You Need to
+            <span className="">Succeed</span>
+          </h2>
+          <p className="">
+            Whether you're looking for your next opportunity or the perfect
+            candidate, we have the tools and features to make it happen.
+          </p>
+        </div>
+
+        <div className="">
+          {/* Job Seekers Section */}
+          <div>
+            <div className="">
+              <h3 className="">For Job Seekers</h3>
+              <div className="" />
+            </div>
+
+            <div className="">
+              {jobSeekerFeatures.map((feature, index) => (
+                <div key={index} className="">
+                  <div className="">
+                    <feature.icon className="" />
+                  </div>
+                  <div>
+                    <h4 className="">{feature.title}</h4>
+                    <p className="">{feature.description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Employers Section*/}
+          <div>
+            <div className="">
+              <h3 className="">For Employers</h3>
+              <div className="" />
+            </div>
+
+            <div className="">
+              {employerFeatures.map((feature, index) => (
+                <div key={index} className="">
+                  <div className="">
+                    <feature.icon className="" />
+                  </div>
+                  <div>
+                    <h4 className="">{feature.title}</h4>
+                    <p className="">{feature.description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Features
