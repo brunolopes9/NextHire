656ab11412df6a825d906b746b6eaeb6d504c3d0
Bruno Lopes <bruno-lopes9@hotmail.com>
2025-08-20 17:30:53 +0100

    Add SignUp first code

--- frontend/NextHire/src/pages/Auth/SignUp.jsx
+++ frontend/NextHire/src/pages/Auth/SignUp.jsx
@@ -1,7 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
+import { motion } from "framer-motion"
+import {
+  User,
+  Mail,
+  Lock,
+  Upload,
+  Eye,
+  EyeOff,
+  UserCheck,
+  Building2,
+  CheckCircle,
+  AlertCircle,
+  Loader,
+} from "lucide-react"
 
 const SignUp = () => {
-  return <div> SignUp </div>
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    role: "",
+    avatar: null,
+  })
+
+  const [formState, setFormState] = useState({
+    loading: false,
+    errors: {},
+    showPassword: false,
+    avatarPreview: null,
+    success: false,
+  })
+
+  return <div> signup</div>
 }
 
 export default SignUp
